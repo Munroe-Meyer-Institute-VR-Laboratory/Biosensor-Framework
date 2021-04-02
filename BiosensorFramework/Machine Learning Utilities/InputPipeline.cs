3b8f69d891b10137ebdab31a0205c95547752dac
wsarce <wsarcera@gmail.com>
2021-04-02 15:32:49 -0500

    - Added Predict to MMIVR.BiosensorFramework.MachineLearningUtilities namespace - Added InputPipeline to MMIVR.BiosensorFramework.InputPipeline namespace

---
+++ BiosensorFramework/Machine Learning Utilities/InputPipeline.cs
@@ -0,0 +1,127 @@
+﻿using MathNet.Numerics.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MMIVR.BiosensorFramework.DataProcessing;
+
+namespace MMIVR.BiosensorFramework.InputPipeline
+{
+    class SignalProcessing
+    {
+        public static List<double> ProcessEdaSignal(double[] EdaSignal, double SamplingRate = 4.0)
+        {
+            List<double> Features;
+            if (EdaSignal == null || EdaSignal.Length == 0)
+                Features = new List<double>(new double[15]);
+            else
+            {
+                double[] ScrSignal = TarvainenDetrending.RemoveTrend(EdaSignal, 1500, null);
+                double[] SclSignal = TarvainenDetrending.GetResidual(EdaSignal, ScrSignal);
+                double[] SummedFeatures = HealeyStressDetection.SumFeatures(HealeyStressDetection.GetPeakFeatures(EdaSignal, SamplingRate));
+                Features = new List<double>
+            {
+                EdaSignal.Mean(),
+                EdaSignal.StandardDeviation(),
+                EdaSignal.Min(),
+                EdaSignal.Max(),
+                FeatureExtraction.SignalSlope(
+                    EdaSignal, Array.ConvertAll(Enumerable.Range(0, EdaSignal.Length).ToArray(), Convert.ToDouble)),
+                FeatureExtraction.SignalDynamicRange(EdaSignal),
+                SclSignal.Mean(),
+                SclSignal.StandardDeviation(),
+                ScrSignal.StandardDeviation(),
+                FeatureExtraction.SignalCorrelation(
+                    SclSignal, Array.ConvertAll(Enumerable.Range(0, SclSignal.Length).ToArray(), Convert.ToDouble)),
+                FeatureExtraction.FindLocalMaxima(ScrSignal, ScrSignal.Mean(), 4).Length,
+                SummedFeatures[0],
+                SummedFeatures[1],
+                SummedFeatures[2],
+                FeatureExtraction.SignalSlope(
+                    ScrSignal, Array.ConvertAll(Enumerable.Range(0, ScrSignal.Length).ToArray(), Convert.ToDouble)),
+            };
+            }
+            return Features;
+        }
+        public static List<double> ProcessAccSignal(double[] AccSignal, double SamplingRate = 32.0)
+        {
+            List<double> Features;
+            if (AccSignal == null || AccSignal.Length == 0)
+                Features = new List<double>(new double[4]);
+            else
+            {
+                Features = new List<double>
+            {
+                AccSignal.Mean(),
+                AccSignal.StandardDeviation(),
+                FeatureExtraction.SignalAbsolute(FeatureExtraction.SignalIntegral(AccSignal)),
+                FeatureExtraction.SignalPeakFrequency(AccSignal, SamplingRate),
+            };
+            }
+            return Features;
+        }
+        public static List<double> ProcessTmpSignal(double[] TempSignal)
+        {
+            List<double> Features = null;
+            if (TempSignal == null || TempSignal.Length == 0)
+                Features = new List<double>(new double[6]);
+            else
+                Features = new List<double>
+            {
+                TempSignal.Mean(),
+                TempSignal.StandardDeviation(),
+                TempSignal.Min(),
+                TempSignal.Max(),
+                FeatureExtraction.SignalDynamicRange(TempSignal),
+                FeatureExtraction.SignalSlope(
+                    TempSignal, Array.ConvertAll(Enumerable.Range(0, TempSignal.Length).ToArray(), Convert.ToDouble))
+            };
+
+            return Features;
+        }
+        public static List<double> ProcessPpgSignal(double[] PpgSignal, double SamplingRate = 64.0, double Threshold = 3.5, int PeakSize = 3)
+        {
+            List<double> Features;
+            if (PpgSignal.Length == 0 || PpgSignal == null)
+                Features = new List<double>(new double[18]);
+            else
+            {
+                double[] PpgDer = HealeyStressDetection.FiniteDifference(ref PpgSignal);
+                int[] Beats = FeatureExtraction.FindLocalMaxima(PpgDer, Threshold, PeakSize);
+                Tuple<double[], double[], int, double> PpgFeatures = BloodVolumePulse.GetHeartFeatures(Beats, SamplingRate);
+                List<Tuple<double, double>> FreqBands = new List<Tuple<double, double>>()
+            {
+                Tuple.Create(0.01, 0.04),
+                Tuple.Create(0.05, 0.15),
+                Tuple.Create(0.15, 0.4),
+                Tuple.Create(0.4, 1.0),
+            };
+                List<double[]> HrvFreqEner = FeatureExtraction.SignalFreqBandEnergies(PpgSignal, FreqBands, SamplingRate);
+                List<double> FreqSummation = FeatureExtraction.SignalFreqSummation(HrvFreqEner);
+                List<double> FreqPower = FeatureExtraction.SignalRelativePower(HrvFreqEner, 1024 / 2);
+                Features = new List<double>
+            {
+                PpgFeatures.Item1.Mean(),
+                PpgFeatures.Item1.StandardDeviation(),
+                PpgFeatures.Item2.Mean(),
+                PpgFeatures.Item2.StandardDeviation(),
+                PpgFeatures.Item3,
+                (double)PpgFeatures.Item4,
+                FeatureExtraction.SignalRMS(PpgFeatures.Item2),
+                FeatureExtraction.SignalFreqRatio(FreqSummation[1], FreqSummation[2]),
+                FreqSummation[0],
+                FreqSummation[1],
+                FreqSummation[2],
+                FreqSummation[3],
+                FreqPower[0],
+                FreqPower[1],
+                FreqPower[2],
+                FreqPower[3],
+                FeatureExtraction.SignalFreqNormalize(HrvFreqEner[1]).Mean(),
+                FeatureExtraction.SignalFreqNormalize(HrvFreqEner[2]).Mean(),
+            };
+            }
+            return Features;
+        }
+    }
+}
---
+++ BiosensorFramework/Machine Learning Utilities/Predict.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ML;
+
+using MMIVR.BiosensorFramework.Extensions;
+using MMIVR.BiosensorFramework.InputPipeline;
+
+namespace MMIVR.BiosensorFramework.MachineLearningUtilities
+{
+    class Predict
+    {
+        /// <summary>
+        /// Takes the readings from the windowed data, extracts the features, and runs it through a prediction pipeline.
+        /// </summary>
+        /// <param name="mlContext"></param>
+        /// <param name="Model"></param>
+        /// <param name="WindowReadings"></param>
+        public static PredictionBinResult PredictWindow(MLContext mlContext, ITransformer Model, List<List<double>> WindowReadings)
+        {
+            List<double> Features = new List<double>();
+            // Since ACC readings can be jagged, i.e. not exactly five seconds of readings, we need to handle
+            // the end index case.  It adds processing time but prevents runtime errors.
+            // If we calculate the number of windows that are present, we can append all of the missing samples after
+            // the processing is complete.
+            // By checking this, we truncate the readings at the end of the array, regardless of how close it is to
+            // a full second of readings.  This was attempted to be handled by the '?' operator in the Range operator
+            // but it would not be implemented in this case since are only dealing with full windows.
+            // TODO: Incorporate readings at the end of the array that are not a full window, the code is there, but the 
+            // check on line 32 needs to be updated to round from midpoint, i.e. 4.45 -> 4 and 4.55 -> 5 
+            // So, actually, we had to create a new 'range' operator because Unity 2019.2.16f doesn't support C# 8.0
+            // and this new extension method, 'GetSubArray', handles end cases by adjusting the end value to the end of the array
+            // By adding in the rounding function, it essentially cuts out a window if there is less than half of the window present
+            int WindowCount = (int)Math.Round(WindowReadings[0].ToArray().Length / 96.0, 0, MidpointRounding.AwayFromZero);
+            WindowCount = WindowCount > 5 ? 5 : WindowCount;
+            for (int i = 0; i < WindowCount; i++)
+            {
+                Features.AddRange(SignalProcessing.ProcessAccSignal(WindowReadings[0].ToArray().GetSubArray(i * 96, (i + 1) * 96)));
+                Features.AddRange(SignalProcessing.ProcessAccSignal(WindowReadings[1].ToArray().GetSubArray(i * 32, (i + 1) * 32)));
+                Features.AddRange(SignalProcessing.ProcessAccSignal(WindowReadings[2].ToArray().GetSubArray(i * 32, (i + 1) * 32)));
+                Features.AddRange(SignalProcessing.ProcessAccSignal(WindowReadings[3].ToArray().GetSubArray(i * 32, (i + 1) * 32)));
+            }
+            for (int i = 0; i < (5 - WindowCount) * 4; i++)
+            {
+                Features.AddRange(new double[4]);
+            }
+            Features.AddRange(SignalProcessing.ProcessPpgSignal(WindowReadings[4].ToArray()));
+            Features.AddRange(SignalProcessing.ProcessEdaSignal(WindowReadings[5].ToArray()));
+            Features.AddRange(SignalProcessing.ProcessTmpSignal(WindowReadings[6].ToArray()));
+
+            ExtractedBinFeatures WindowFeatures = new ExtractedBinFeatures()
+            {
+                Features = Features.ToArray().ToFloat(),
+            };
+            PredictionBinResult Prediction = MakeBinPrediction(mlContext, WindowFeatures, Model);
+            return Prediction;
+        }
+        public static PredictionBinResult MakeBinPrediction(MLContext mlContext, ExtractedBinFeatures LiveData, ITransformer Model)
+        {
+            return mlContext.Model.CreatePredictionEngine<ExtractedBinFeatures, PredictionBinResult>(Model).Predict(LiveData);
+        }
+    }
+}
